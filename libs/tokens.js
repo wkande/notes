f03b23b3976425b9949277e92e6df288ea28158d
Warren Anderson <warren@wyomingsoftware.com>
2020-11-03 13:48:05 -0700

    token access to early

--- libs/tokens.js
+++ libs/tokens.js
@@ -8,10 +8,13 @@ module.exports.getToken = function(email){
 
 module.exports.validateToken = function(auth){
   try{
-    console.log('validateToken => token:', token);
-    console.log('validateToken => jwt.verify:', jwt.verify(token, process.env.JWT_SECRET));
+    
     // auth param has Bearer attached
     const token = auth.split(' ')[1];
+
+    console.log('validateToken => token:', token);
+    console.log('validateToken => jwt.verify:', jwt.verify(token, process.env.JWT_SECRET));
+    
     // The decoded token is just the email
     return jwt.verify(token, process.env.JWT_SECRET);
   }
